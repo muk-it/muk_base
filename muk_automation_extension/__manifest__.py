5356efb8cfbed26cdfff82de18f325a22d488f52
MuK IT GmbH <mathias.markl@mukit.at>
2018-07-29 09:57:31 +0000

    publish - 11.0.1.0.1

--- muk_automation_extension/__manifest__.py
+++ muk_automation_extension/__manifest__.py
@@ -20,7 +20,7 @@
 {
     "name": "MuK Automation Extension",
     "summary": """Extension for Odoo's Base Automation""",
-    "version": '11.0.1.0.0',   
+    "version": '11.0.1.0.1',   
     "category": 'Extra Tools',   
     "license": "AGPL-3",
     "website": "https://www.mukit.at",
