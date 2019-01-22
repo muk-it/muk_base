ed02ffcfff5e6da6e0d47740bed8a927ed16feac
MuK IT GmbH <mathias.markl@mukit.at>
2019-01-22 15:24:08 +0000

    publish muk_dbfilter - 12.0

---
+++ muk_dbfilter/__init__.py
@@ -0,0 +1,25 @@
+###################################################################################
+#
+#    Copyright (C) 2018 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+from odoo.tools import config
+
+def _patch_system():
+    server_modules = config.get('server_wide_modules') or []
+    if 'muk_dbfilter' in server_modules and config.get('proxy_mode'):
+        from . import patch
---
+++ muk_dbfilter/__manifest__.py
@@ -0,0 +1,51 @@
+###################################################################################
+#
+#    Copyright (C) 2018 MuK IT GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###################################################################################
+
+{
+    "name": "MuK DB Filter",
+    "summary": """Headers based Database Selection""",
+    "version": "11.0.1.0.1",
+    'category': 'Extra Tools',
+    'license': 'AGPL-3',
+    'author': 'MuK IT',
+    'website': 'https://www.mukit.at',
+    'live_test_url': 'https://demo.mukit.at/web/login',
+    "depends": [
+    ],
+    "contributors": [
+        "Kerrim Abdelhamed <kerrim.abdelhamed@mukit.at>",
+        "Mathias Markl <mathias.markl@mukit.at>"
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ],
+    "images": [
+    ],
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+    "post_load": "_patch_system",
+}
