d8e18ee33c9992fb48c8fa2b0bd6710f4c6e9ba1
MuK IT GmbH <mathias.markl@mukit.at>
2019-06-23 19:54:10 +0000

    publish muk_utils - 12.0

--- muk_utils/__manifest__.py
+++ muk_utils/__manifest__.py
@@ -19,7 +19,7 @@
 { 
     "name": "MuK Utils",
     "summary": """Utility Features""",
-    "version": '12.0.1.6.26',   
+    "version": '12.0.1.6.27',  
     "category": 'Extra Tools',   
     "license": "AGPL-3",
     "author": "MuK IT",
