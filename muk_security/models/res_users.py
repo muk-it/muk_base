f6ef05f1279c5f2cd6e05ed145381f5015ee446e
Dmytro Katyukha <cto@crnd.pro>
2019-07-04 19:32:57 +0300

    Fix muk_security

--- muk_security/models/res_users.py
+++ muk_security/models/res_users.py
@@ -1,5 +1,5 @@
 ###################################################################################
-# 
+#
 #    Copyright (C) 2017 MuK IT GmbH
 #
 #    This program is free software: you can redistribute it and/or modify
