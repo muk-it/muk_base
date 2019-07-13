b92878314d6d3fb4332923eb9b6e22b46977a130
MuK IT GmbH <mathias.markl@mukit.at>
2019-07-13 00:32:58 +0000

    publish muk_fields_file - 12.0

--- muk_fields_file/__manifest__.py
+++ muk_fields_file/__manifest__.py
@@ -23,7 +23,7 @@
 {
     "name": "MuK Filestore Field",
     "summary": """Filestore Support for Fields""",
-    "version": '12.0.2.0.0',   
+    "version": '12.0.2.0.1',   
     "category": 'Extra Tools',   
     "license": "LGPL-3",
     "website": "https://www.mukit.at",
