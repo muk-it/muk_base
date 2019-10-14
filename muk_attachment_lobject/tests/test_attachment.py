ed9598ac243a3a31a3630c0b59dabc2a54fc9a0c
MuK IT GmbH <mathias.markl@mukit.at>
2019-10-14 08:51:36 +0000

    publish muk_attachment_lobject - 12.0

--- muk_attachment_lobject/tests/test_attachment.py
+++ muk_attachment_lobject/tests/test_attachment.py
@@ -2,7 +2,7 @@
 #
 #    Copyright (c) 2017-2019 MuK IT GmbH.
 #
-#    This file is part of MuK Large Objects Attachment
+#    This file is part of MuK Large Objects Attachment 
 #    (see https://mukit.at).
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -28,38 +28,38 @@ from odoo.tests import common
 
 _logger = logging.getLogger(__name__)
 
-
 class AttachmentTestCase(common.HttpCase):
+    
     def setUp(self):
         super(AttachmentTestCase, self).setUp()
-        self.attachment = self.env["ir.attachment"].sudo()
-        self.params = self.env["ir.config_parameter"].sudo()
-        self.location = self.params.get_param("ir_attachment.location")
-        self.params.set_param("ir_attachment.location", "lobject")
+        self.attachment = self.env['ir.attachment'].sudo()
+        self.params = self.env['ir.config_parameter'].sudo()
+        self.location = self.params.get_param('ir_attachment.location')
+        self.params.set_param('ir_attachment.location', 'lobject')
 
     def tearDown(self):
-        self.params.set_param("ir_attachment.location", self.location)
+        self.params.set_param('ir_attachment.location', self.location)
         super(AttachmentTestCase, self).tearDown()
-
+    
     def test_attachment(self):
-        attach = self.attachment.create(
-            {"name": "Test", "datas": base64.b64encode(b"\xff data")}
-        )
+        attach = self.attachment.create({
+            'name': "Test",
+            'datas': base64.b64encode(b"\xff data")})
         self.assertTrue(attach.datas)
         self.assertTrue(attach.store_lobject)
-        self.assertTrue(attach.with_context({"bin_size": True}).datas)
-        self.assertTrue(attach.with_context({"bin_size": True}).store_lobject)
-        self.assertTrue(attach.with_context({"human_size": True}).store_lobject)
-        self.assertTrue(attach.with_context({"base64": True}).store_lobject)
-        self.assertTrue(attach.with_context({"stream": True}).store_lobject)
-        oid = attach.with_context({"oid": True}).store_lobject
+        self.assertTrue(attach.with_context({'bin_size': True}).datas)
+        self.assertTrue(attach.with_context({'bin_size': True}).store_lobject)
+        self.assertTrue(attach.with_context({'human_size': True}).store_lobject)
+        self.assertTrue(attach.with_context({'base64': True}).store_lobject)
+        self.assertTrue(attach.with_context({'stream': True}).store_lobject)
+        oid = attach.with_context({'oid': True}).store_lobject
         self.assertTrue(oid)
-        attach.write({"datas": base64.b64encode(b"\xff data")})
-        self.assertTrue(oid != attach.with_context({"oid": True}).store_lobject)
-        self.assertTrue(attach.export_data(["datas"]))
-        self.assertTrue(attach.export_data(["datas"], raw_data=True))
+        attach.write({'datas': base64.b64encode(b"\xff data")})
+        self.assertTrue(oid != attach.with_context({'oid': True}).store_lobject)
+        self.assertTrue(attach.export_data(['datas']))
+        self.assertTrue(attach.export_data(['datas'], raw_data=True))
         attach.unlink()
-
+    
     @unittest.skip("The test takes a long time and is therefore skipped by default.")
     def test_migration(self):
         self.attachment.force_storage()
